ce1ed8a944b86635b1c6e5ff026f13ddc4e065c9
eugeneus <yaugeny@yahoo.com>
2014-12-23 08:29:04 +0300

    update level provider - initially

--- common/util/LevelDataProvider.h
+++ common/util/LevelDataProvider.h
@@ -10,6 +10,13 @@
 
 #include "cocos2d.h"
 
+// should move to gift factory?
+struct GiftDesc{
+    int productsCount; // number of distinct Proudcts in a Gift Box
+    std::vector<int> productList; // list of distinct product id's
+    int giftsPerLevel;  // how may times given gift should appears on the level
+};
+
 class LevelDataProvider : public cocos2d::Node {
 
 public:
@@ -28,13 +35,58 @@ protected:
 	std::string _levelName;
 	
 	std::string _backgroundImageName;
+    
+    //
+    // TODO: image names for other static stuff, aka decorations of level's scene
+    // std::string _cheatImageName;
+    // std::string _christmasTreeImageName;
+    // std::string _productHeapImageName;
+    // std::string _carpetImageName;
+    // etc ...
+    //
+    
+    // product section
+    
+    std::vector<int> _randomProductsQueue; // list of ID,s of vatious items which are not a part
+                                           // of any Present determines order of occurance
+    
+    std::vector<GiftDesc> _giftsQueue; // list of gifts, determones order fo occurance
+    
+
+    //GiftBox
+    std::vector<int> _giftBoxList; // list of gifts, determones order fo occurance
+    
+    // tossingActor
+    std::vector<int> _tossingActorList; // list of IDs of carrier actors which sould appear on the Level
+    std::vector<cocos2d::Point> _tossingActorPositionList; // list relative position for given tossing Actor
+    
+    // carrierActor
+    std::vector<int> _carrierActorList; // list of IDs of carrier actors which sould appear on the Level
+    
+    // walkingActor
+    std::vector<int> _walkingActorList; // list of IDs of walking actors which sould appear on the Level
+    
+    
+    // list of
+    //GiftDesc
+    // giftID
+    
+    // listOf random Products (not a gift stuff)
+    // productID
+    
+    
+    //record giftDesc:
+    // int totalProducts // count of distinct Products in the Gift
+    // std::vector<int> productList  // list of distinct Product's IDs
+    // int giftsPerLEvel // count of the gift of given type on the level
+    //std::vector<>
 	
 	
 	// for actors and stuffs we will not store image names in level:
 	// it will be number of types (id)
 	// even though the behaviour and other will be the same,
     // by ID we may employ different set of images,
-    // so we may have lots of different looking stuff (actors, products, etc.)	
+    // so we may have lots of different looking stuff (actors, products, etc.)
 	
 	// game model will ask for types from level plist
 	// then call suitable factory class
@@ -54,22 +106,6 @@ protected:
 	// but there are another issue: some part may have anchor shifted from the center 
 	// need to be investigated!
 	
-	// carrierActor
-	// std::string _imageNameBase;
-	// tossingActor
-	// std::string _imageNameBase;
-	// walkingActor
-	// std::string _imageNameBase;
-	
-	//GiftBox
-	// std::string _baseImageName;
-	
-	// list of
-	//GiftDesc
-	// giftID
-	
-	// listOf random Products (not a gift stuff)
-	// productID
 
 private:
     
